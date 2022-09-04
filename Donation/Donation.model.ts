38026c96a37c71af4b62135f0761a92d82e83ef3
Vihaan Verma <vverma270705@gmail.com>
2022-09-04 10:08:21 +0530

    webhook error

--- Donation/Donation.model.ts
+++ Donation/Donation.model.ts
@@ -2,14 +2,7 @@ import { model, Schema } from "mongoose";
 import { IDonation } from "./Donation.interfaces";
 
 const DonationSchema = new Schema<IDonation>({
-  amount: {
-    type: Number,
-    required: true,
-  },
-  transactionId: { type: String, required: true },
-  type: { type: String, required: true },
-  // identifier: { type: String, required: true },
-  verified: { type: Boolean, default: false },
+  donation: Schema.Types.Mixed,
 });
 
 const Donation = model<IDonation>("Donation", DonationSchema);
